6f041717f417d8e620d7efbc635d68d457042963
ManjunathanRajan <manjun566@gmail.com>
2023-04-28 18:31:38 +0530

    shell script

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+# download and install packer
+echo "- Installing packer"
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install packer
+
+# run packer
+echo "- Running packer"
+/usr/bin/packer build packer.json
